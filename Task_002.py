666f03e99c4f7cd0993806a4e678bf93b33fe6ed
mike77 <weilliss@mail.ru>
2023-04-09 17:26:16 +0600

    Второе решение

--- Task_002.py
+++ Task_002.py
@@ -6,12 +6,14 @@
 # 100 -> 1 (1 + 0 + 0) |
 
 
-num = int(input())
-sum = 0
-while num > 0:
-    sum += int(num % 10)
-    num /= 10
+# num = int(input())
+# sum = 0
+# while num > 0:
+#     sum += int(num % 10)
+#     num /= 10
  
-print(sum)
+# print(sum)
 
-
+a=input()
+b=map(int, str(a))
+print(sum(b))
