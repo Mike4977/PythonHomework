def369b2da9cce22a5dcf19aef09deeaae67f322
mike-13 <weilliss@mail.ru>
2023-04-30 15:31:46 +0600

    из эталооного решения

--- Task_030.py
+++ Task_030.py
@@ -5,22 +5,29 @@
 # Каждое число вводится с новой строки.
 
 
-def progression(a, d, n):
-    arr = [a]
-    for i in range(2, n + 1):
-        a1 = a + d * (i - 1)
-        arr.append(a1)
-    return arr
+# def progression(a, d, n):
+#     arr = [a]
+#     for i in range(2, n + 1):
+#         a1 = a + d * (i - 1)
+#         arr.append(a1)
+#     return arr
 
 
-def progression(a, d, n):
-    arr = []
-    for i in range(n):
-        arr.append(a + d * (i - 1))
-    return arr
+# def progression(a, d, n):
+#     arr = []
+#     for i in range(n):
+#         arr.append(a + d * (i - 1))
+#     return arr
 
-a = int(input("Введите первый элемент прогрессии a = "))
-d = int (input("Введите разность элементов d = "))
-n = int(input("Введите количество элементов n = "))
+a1 = int(input())
+d = int(input())
+n = int(input())
+for i in range(n):
+    print(a1 + i * d)
 
-print(progression(a, d, n))
+
+# a = int(input("Введите первый элемент прогрессии a = "))
+# d = int (input("Введите разность элементов d = "))
+# n = int(input("Введите количество элементов n = "))
+
+# print(progression(a, d, n))
