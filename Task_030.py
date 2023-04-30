f438ef7682a2cc928e0c5f9afc33fbedd26a8dd0
mike-13 <weilliss@mail.ru>
2023-04-30 15:29:48 +0600

    вариант 2

--- Task_030.py
+++ Task_030.py
@@ -12,6 +12,13 @@ def progression(a, d, n):
         arr.append(a1)
     return arr
 
+
+def progression(a, d, n):
+    arr = []
+    for i in range(n):
+        arr.append(a + d * (i - 1))
+    return arr
+
 a = int(input("Введите первый элемент прогрессии a = "))
 d = int (input("Введите разность элементов d = "))
 n = int(input("Введите количество элементов n = "))
