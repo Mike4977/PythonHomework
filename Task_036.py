fded4b1858467d0e0da1f3be8b473cf1526716d0
Mike4977 <weilliss@mail.ru>
2023-05-25 19:24:23 +0600

    оптимизация

--- Task_036.py
+++ Task_036.py
@@ -19,10 +19,15 @@
 
 
 def print_operation_table(operation, num_rows=6, num_columns=6):
-    for row in range(1, num_rows+1):
-        for column in range(1, num_columns+1):
-            print(operation(row,column), end = '\t')
-        print()
+    # for row in range(1, num_rows+1):
+    #     for column in range(1, num_columns+1):
+    #         print(operation(row,column), end = '\t')
+    #     print()
+
+        a = [[operation(i, j) for j in range(1, num_columns + 1)] for i in range(1, num_rows + 1)]
+        for i in a:
+            print(*[f"{x:>3}" for x in i])
+    
 
 print_operation_table(lambda x, y: x * y)
 
