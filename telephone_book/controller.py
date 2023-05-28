8e58a3114aa277ffe33432065ab94021f611fccb
Mike4977 <weilliss@mail.ru>
2023-05-28 21:57:36 +0600

    Телефонная книга

--- telephone_book/controller.py
+++ telephone_book/controller.py
@@ -9,13 +9,13 @@ def main():
             data = view.people()
             database.added(data)
         elif ask == 2:
+            find = view.search()
             database.search(find)
         elif ask == 5:
             database.read()
         elif ask == 0:
             break
         # elif ask == 4:
-
         # elif ask == 5:
             # str = view.input_main()
 
